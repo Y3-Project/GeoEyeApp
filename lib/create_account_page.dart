9074a54375e3673c716a8bc5683a38b2273f0dc1
ppk200 <ppk2000@hw.ac.uk>
2023-01-06 18:28:11 +0000

    made modifications

--- lib/create_account_page.dart
+++ lib/create_account_page.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app_firebase_login/decision_tree.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_app_firebase_login/login_page.dart';
 
 import 'home_page.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_firebase_login/decision_tree.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_app_firebase_login/welcome_page.dart';
 
