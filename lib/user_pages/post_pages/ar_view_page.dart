006fe2b9e4d66356a2bffdf7651c9b0d5139dc56
Lukas-Jurcaga <ljurcaga@gmail.com>
2023-01-25 14:49:27 +0000

    Added Appbar to notifications and profile page

---
+++ lib/user_pages/post_pages/ar_view_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ARViewPage extends StatefulWidget {
+  const ARViewPage({Key? key}) : super(key: key);
+
+  @override
+  State<ARViewPage> createState() => _ARViewPageState();
+}
+
+class _ARViewPageState extends State<ARViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
