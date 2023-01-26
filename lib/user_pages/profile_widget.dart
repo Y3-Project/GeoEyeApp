f61164bf04a8ac22fc3173a41be5cd22ba7b68be
ppk200 <ppk2000@hw.ac.uk>
2023-01-26 16:23:52 +0000

    removed password and privacy settings pages; logout button in account settings page, added links for account settings button and help button

--- lib/user_pages/profile_widget.dart
+++ lib/user_pages/profile_widget.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_firebase_login/user_pages/settings_pages/account_settings_page.dart';
+import 'package:flutter_app_firebase_login/user_pages/settings_pages/help_page.dart';
 import '../login_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -24,9 +26,7 @@ class ProfileWidget extends StatelessWidget {
       );
     }
 
-
-    String? getUuid()
-    {
+    String? getUuid() {
       String? uuid = '';
       final FirebaseAuth auth = FirebaseAuth.instance;
       final User? user = auth.currentUser;
@@ -36,18 +36,18 @@ class ProfileWidget extends StatelessWidget {
     }
 
     //method to get the username of the user logged in
-    Future<String> getUsername() async
-    {
-        String username = '';
-        QuerySnapshot<Map<String, dynamic>> snap = await FirebaseFirestore
-            .instance.collection("users")
-            .where("uuid", isEqualTo: getUuid() as String)
-            .get();
-        List<QueryDocumentSnapshot<Map<String, dynamic>>> docList = snap.docs;
-        for (QueryDocumentSnapshot<Map<String, dynamic>> doc in docList) {
-          username = doc.get('username');
-        }
-        return await username;
+    Future<String> getUsername() async {
+      String username = '';
+      QuerySnapshot<Map<String, dynamic>> snap = await FirebaseFirestore
+          .instance
+          .collection("users")
+          .where("uuid", isEqualTo: getUuid() as String)
+          .get();
+      List<QueryDocumentSnapshot<Map<String, dynamic>>> docList = snap.docs;
+      for (QueryDocumentSnapshot<Map<String, dynamic>> doc in docList) {
+        username = doc.get('username');
+      }
+      return await username;
     }
 
     getUsername().then((value) => {username = value});
@@ -59,122 +59,119 @@ class ProfileWidget extends StatelessWidget {
         toolbarHeight: 64,
         backgroundColor: Colors.black,
         centerTitle: true,
-        title: Text("Profile Page"),
+        title: Text("Profile Page", style: TextStyle(fontSize: 25),),
       ),
       body: Container(
-          alignment: Alignment.center,
-          margin: const EdgeInsets.fromLTRB(10, 20, 10, 10),
-          decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.white),
-              borderRadius: const BorderRadius.all(Radius.circular(20))),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                          height: 100,
-                          width: 100,
-                          margin: const EdgeInsets.fromLTRB(0, 0, 10, 10),
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(color: Colors.black)),
-                          child: Icon(
-                            Icons.person,
-                            size: 80,
-                          ),
-                        ),
-                        Text(username,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16)),
-                      ],
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          logout();
-                        },
-                        child: const Text(
-                          'Logout',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 15),
-                        ))
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black)),
-                      child: Text("Post Count"),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black)),
-                      child: Text("Interactions"),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black)),
-                      child: Text("Interacted with"),
-                    )
-                  ],
-                )
-              ]),
-              Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+        alignment: Alignment.center,
+        margin: const EdgeInsets.fromLTRB(10, 20, 10, 10),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.white),
+            borderRadius: const BorderRadius.all(Radius.circular(20))),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: SETTINGS_BUTTON_SPACING),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size.fromHeight(
-                              SETTINGS_BUTTON_WIDTH), // fromHeight use double.infinity as width and 40 is the height
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        margin: const EdgeInsets.fromLTRB(0, 0, 10, 10),
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(color: Colors.black)),
+                        child: Icon(
+                          Icons.person,
+                          size: 80,
                         ),
-                        onPressed: () {},
-                        child: Text("Password Settings")),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: SETTINGS_BUTTON_SPACING),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size.fromHeight(
-                              SETTINGS_BUTTON_WIDTH), // fromHeight use double.infinity as width and 40 is the height
-                        ),
-                        onPressed: () {},
-                        child: Text("Account Settings")),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: SETTINGS_BUTTON_SPACING),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size.fromHeight(
-                              SETTINGS_BUTTON_WIDTH), // fromHeight use double.infinity as width and 40 is the height
-                        ),
-                        onPressed: () {},
-                        child: Text("Privacy Settings")),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: SETTINGS_BUTTON_SPACING),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size.fromHeight(
-                              SETTINGS_BUTTON_WIDTH), // fromHeight use double.infinity as width and 40 is the height
-                        ),
-                        onPressed: () {},
-                        child: Text("Help")),
+                      ),
+                      Text(username,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20)),
+                    ],
                   ),
                 ],
               ),
-            ],
-          )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    decoration:
+                        BoxDecoration(border: Border.all(color: Colors.black)),
+                    child: Text("Post Count",
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  ),
+                  Container(
+                    decoration:
+                        BoxDecoration(border: Border.all(color: Colors.black)),
+                    child: Text("Interactions",
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  ),
+                  Container(
+                    decoration:
+                        BoxDecoration(border: Border.all(color: Colors.black)),
+                    child: Text("Interacted with",
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  )
+                ],
+              )
+            ]),
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  margin:
+                      EdgeInsets.symmetric(vertical: SETTINGS_BUTTON_SPACING),
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        minimumSize: Size.fromHeight(
+                            SETTINGS_BUTTON_WIDTH), // fromHeight use double.infinity as width and 40 is the height
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => AccountSettingsPage()
+                          ),
+                        );
+                      },
+                      child: Text(
+                        "Account Settings",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 22),
+                      )),
+                ),
+                Container(
+                  margin:
+                      EdgeInsets.symmetric(vertical: SETTINGS_BUTTON_SPACING),
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        minimumSize: Size.fromHeight(
+                            SETTINGS_BUTTON_WIDTH), // fromHeight use double.infinity as width and 40 is the height
+                      ),
+                      onPressed: () {Navigator.of(context).push(
+                        MaterialPageRoute(
+                            builder: (context) => HelpPage()
+                        ),
+                      );},
+                      child: Text(
+                        "Help",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 22),
+                      )),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/user_pages/settings_pages/account_settings_page.dart
+++ lib/user_pages/settings_pages/account_settings_page.dart
@@ -1,11 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../login_page.dart';
 
 class AccountSettingsPage extends StatelessWidget {
   const AccountSettingsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    //method to log the user out
+    Future<void> logout() async {
+      await FirebaseAuth.instance.signOut();
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => LoginPage(onSignIn: (user) {}),
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: Title(
+              color: Colors.black,
+              child: Text(
+                "Account Settings",
+                style: TextStyle(fontSize: 25),
+              ))),
+      body: ElevatedButton(
+          style: ButtonStyle(
+              backgroundColor:
+              MaterialStatePropertyAll(Colors.black)),
+          onPressed: () {
+            logout();
+          },
+          child: const Text(
+            'Logout',
+            style: TextStyle(
+                fontWeight: FontWeight.bold, fontSize: 18),
+          )),
+    );
   }
 }
