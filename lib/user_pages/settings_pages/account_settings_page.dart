09189840ac2f8f3fff35cdc220fac257d317a486
ppk200 <ppk2000@hw.ac.uk>
2023-01-29 21:36:42 +0000

    user now gets deleted from Firestore Database when they press Delete Account in Account Settings

--- lib/user_pages/settings_pages/account_settings_page.dart
+++ lib/user_pages/settings_pages/account_settings_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -20,7 +21,16 @@ class AccountSettingsPage extends StatelessWidget {
       );
     }
 
+    Future<void> deleteUser() async {
+      //delete from Firebase Authentication
+      final FirebaseAuth auth = FirebaseAuth.instance;
+      final User? user = auth.currentUser;
+      user?.delete();
 
+      //delete from Firestore Database
+      final _db = FirebaseFirestore.instance;
+      await _db.collection("users").doc(user?.uid).delete();
+    }
 
     //pop up dialog when 'Delete Account" button pressed
     Future<void> _showMyDialog() async {
@@ -30,11 +40,16 @@ class AccountSettingsPage extends StatelessWidget {
         builder: (BuildContext context) {
           return AlertDialog(
             backgroundColor: Colors.black,
-            title: const Text('Account Deletion', style: TextStyle(color: Colors.white)),
+            title: const Text('Account Deletion',
+                style: TextStyle(color: Colors.white)),
             content: SingleChildScrollView(
               child: ListBody(
                 children: const <Widget>[
-                  Text('Are you sure you want to delete your account?', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 23)),
+                  Text('Are you sure you want to delete your account?',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 23)),
                 ],
               ),
             ),
@@ -42,15 +57,15 @@ class AccountSettingsPage extends StatelessWidget {
               TextButton(
                 child: const Text('Yes'),
                 onPressed: () {
-                  final FirebaseAuth auth = FirebaseAuth.instance;
-                  final User? user = auth.currentUser;
-                  user?.delete();
+                  deleteUser();
                   Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => WelcomePage()),
+                    MaterialPageRoute(builder: (context) => WelcomePage()),
                   );
                   SnackBar snack1 = SnackBar(
-                      content: Text('Your account has been deleted', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)), duration: Duration(seconds: 2));
+                      content: Text('Your account has been deleted',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20)),
+                      duration: Duration(seconds: 2));
                   ScaffoldMessenger.of(context).showSnackBar(snack1);
                 },
               ),
@@ -66,42 +81,37 @@ class AccountSettingsPage extends StatelessWidget {
       );
     }
 
-
-
     return Scaffold(
-      appBar: AppBar(
-          centerTitle: true,
-          title: Title(
-              color: Colors.black,
-              child: Text(
-                "Account Settings",
-                style: TextStyle(fontSize: 25),
-              ))),
-      body: Column(children: [ElevatedButton(
-          style: ButtonStyle(
-              fixedSize: MaterialStatePropertyAll(Size(150, 20)),
-              backgroundColor:
-              MaterialStatePropertyAll(Colors.black)),
-          onPressed: () {
-            logout();
-          },
-          child: const Text(
-            'Logout',
-            style: TextStyle(
-                fontWeight: FontWeight.bold, fontSize: 18),
-          )),
-        ElevatedButton(
-            style: ButtonStyle(
-                backgroundColor:
-                MaterialStatePropertyAll(Colors.black)),
-            onPressed: () {
-              _showMyDialog();
-            },
-            child: const Text(
-              'Delete Account',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold, fontSize: 18),
-            ))])
-    );
+        appBar: AppBar(
+            centerTitle: true,
+            title: Title(
+                color: Colors.black,
+                child: Text(
+                  "Account Settings",
+                  style: TextStyle(fontSize: 25),
+                ))),
+        body: Column(children: [
+          ElevatedButton(
+              style: ButtonStyle(
+                  fixedSize: MaterialStatePropertyAll(Size(150, 20)),
+                  backgroundColor: MaterialStatePropertyAll(Colors.black)),
+              onPressed: () {
+                logout();
+              },
+              child: const Text(
+                'Logout',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              )),
+          ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.black)),
+              onPressed: () {
+                _showMyDialog();
+              },
+              child: const Text(
+                'Delete Account',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ))
+        ]));
   }
 }
