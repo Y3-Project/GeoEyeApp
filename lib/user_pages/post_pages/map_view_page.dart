6b59db81ba8eea4e168dbe4bba085f9c0acec09b
ppk200 <ppk2000@hw.ac.uk>
2023-02-07 20:29:38 +0000

    added basic map using flutter_map plugin

--- lib/user_pages/post_pages/map_view_page.dart
+++ lib/user_pages/post_pages/map_view_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapViewPage extends StatefulWidget {
   const MapViewPage({Key? key}) : super(key: key);
@@ -11,14 +13,23 @@ class MapViewPage extends StatefulWidget {
 class _MapViewPageState extends State<MapViewPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      margin: const EdgeInsets.fromLTRB(0, 5, 0, 10),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.white),
-          borderRadius: const BorderRadius.all(Radius.circular(20))),
-      child: Text("Map View"),
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(51.509364, -0.128928),
+        zoom: 9.2
+      ),
+      nonRotatedChildren: [
+        AttributionWidget.defaultWidget(
+          source: 'OpenStreetMap contributors',
+          onSourceTapped: null,
+        ),
+      ],
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+      ],
     );
   }
 }
