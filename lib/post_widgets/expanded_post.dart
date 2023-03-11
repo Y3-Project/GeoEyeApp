a6c682d45d2bf948d9cd2b46970775b5b0000b81
03y <98747393+03y@users.noreply.github.com>
2023-03-11 17:47:20 +0000

    view comments and likes

--- lib/post_widgets/expanded_post.dart
+++ lib/post_widgets/expanded_post.dart
@@ -24,6 +24,11 @@ class _ExpandedPostPageState extends State<ExpandedPostPage> {
 
   var comments = List.empty(growable: true);
 
+  SnackBar likedSnackBar = SnackBar(
+    content: Text("You liked this post"),
+    duration: Duration(seconds: 1),
+  );
+
   @override
   void initState() {
     _querySnapshot = FirebaseFirestore.instance
@@ -47,17 +52,59 @@ class _ExpandedPostPageState extends State<ExpandedPostPage> {
   }
 
   Future<void> initDisplay(int i) async {
-    // print("comments: ${_snapshots[i].data().toString()}");
-    // _display.add(await );
-
-    // convert snapshot data to array of comments
     for (int i = 0; i < _snapshots.length; i++) {
       comments.add(new Comment(
-        content: _snapshots[i].get('content'),
-        post: _snapshots[i].get('post').toString(),
-        reports: _snapshots[i].get('reports'),
-        user: _snapshots[i].get('user').toString(),
-      ));
+          content: _snapshots[i].get('content'),
+          post: _snapshots[i].get('post').toString(),
+          reports: _snapshots[i].get('reports'),
+          user: _snapshots[i].get('user').toString(),
+          timestamp: _snapshots[i].get('timestamp')));
+    }
+  }
+
+  /* TODO: this doesnt work idk why
+  String getUsername(String documentReference) {
+    RegExp pattern = new RegExp('users\/([a-zA-Z0-9]+)');
+    String? match = pattern.stringMatch(documentReference)?.split("/")?.last;
+
+    print(match);
+
+    if (match != null) {
+      // get username from user document
+      FirebaseFirestore.instance
+          .collection("users")
+          .doc("general")
+          .get()
+          .then((value) {
+        return value.get("username") == null ? "Unknown user" : value.get("username");
+      });
+    }
+    return "unknown user";
+  }
+   */
+
+  String getLikesString(List<dynamic> likes) {
+    if (likes.length == 0) {
+      return "Be the first to like this post!";
+    } else if (likes.length == 1) {
+      return "Liked by " + likes[0].toString();
+    } else if (likes.length == 2) {
+      return "Liked by " + likes[0].toString() + " and " + likes[1].toString();
+    } else if (likes.length == 3) {
+      return "Liked by " +
+          likes[0].toString() +
+          ", " +
+          likes[1].toString() +
+          " and " +
+          likes[2].toString();
+    } else {
+      return "Liked by " +
+          likes[0].toString() +
+          ", " +
+          likes[1].toString() +
+          " and " +
+          (likes.length - 3).toString() +
+          " others";
     }
   }
 
@@ -91,11 +138,35 @@ class _ExpandedPostPageState extends State<ExpandedPostPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(widget.post.likes.length.toString() + " likes",
-                  style: TextStyle(fontSize: 20)),
-              Image.network(widget.post.picture == ""
-                  ? "https://www.myutilitygenius.co.uk/assets/images/blogs/default-image.jpg"
-                  : widget.post.picture),
+              InkWell(
+                onDoubleTap: () => {
+                  print('double tapped'),
+                  ScaffoldMessenger.of(context).showSnackBar(likedSnackBar)
+                },
+                child: Image.network(widget.post.picture == ""
+                    ? "https://www.myutilitygenius.co.uk/assets/images/blogs/default-image.jpg"
+                    : widget.post.picture),
+              ),
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: InkWell(
+                  onTap: () => {
+                    print('clicked like'),
+                    ScaffoldMessenger.of(context).showSnackBar(likedSnackBar)
+                  },
+                  borderRadius: BorderRadius.circular(8),
+                  child: Container(
+                    height: 40,
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(getLikesString(widget.post.likes),
+                          style: TextStyle(fontSize: 20)),
+                    ),
+                  ),
+                ),
+              ),
               Align(
                 alignment: Alignment.centerLeft,
                 child: Text(
@@ -106,12 +177,26 @@ class _ExpandedPostPageState extends State<ExpandedPostPage> {
                         TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
               ),
               for (int i = 0; i < comments.length; i++)
-                Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                        "    " +
-                            comments[i].content,
-                        style: TextStyle(fontSize: 20))),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(8),
+                    onTap: () => print('clicked on comment: ' + i.toString()), // go to user profile
+                    onLongPress: () => print('long pressed comment: ' + i.toString()), // report comment (maybe open a menu first?)
+                    child: Container(
+                      height: 50,
+                      child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                              "user" + i.toString() + //getUsername(comments[i].user).toString() +
+                                  ": " +
+                                  comments[i].content,
+                              style: TextStyle(fontSize: 20))),
+                    ),
+                  ),
+                ),
             ],
           ),
         ),
--- lib/util/comment.dart
+++ lib/util/comment.dart
@@ -7,13 +7,15 @@ class Comment {
       growable:
           true); // this is an array of refs to users who have reported the post
   String user = ''; // the author of the comment
+  Timestamp timestamp = Timestamp.fromMillisecondsSinceEpoch(0);
 
   /* Constructor */
   Comment(
       {required this.content,
       required this.post,
       required this.reports,
-      required this.user});
+      required this.user,
+      required this.timestamp});
 
   /* Convert a document to a Comment object */
   Comment.fromDocument(DocumentSnapshot doc) {
@@ -21,5 +23,6 @@ class Comment {
     this.post = doc['post'].toString();
     this.reports = doc['reports'];
     this.user = doc['user'].toString();
+    this.timestamp = doc['timestamp'];
   }
 }
