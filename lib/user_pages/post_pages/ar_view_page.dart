5b80b05730cc28e886bf7925b881ce014f71f8df
Jacob Turner <jacobmturner@gmail.com>
2023-03-13 11:44:52 +0000

    populate stack with scrapbooks
    there are 2 main problems
    - displaying the scrapbook(s) at the correct location compared to the camera(!)
    - camera doesnt take up the whole width to me and that's annoying

--- lib/user_pages/post_pages/ar_view_page.dart
+++ lib/user_pages/post_pages/ar_view_page.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
 import 'package:camera/camera.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_firebase_login/scrapbook_widgets/scrapbook.dart';
+import 'package:flutter_app_firebase_login/scrapbook_widgets/scrapbook_tile.dart';
 
 class ARViewPage extends StatefulWidget {
   const ARViewPage({Key? key}) : super(key: key);
@@ -12,10 +17,15 @@ class _ARViewPageState extends State<ARViewPage> {
   CameraController? cameraController; // controller for camera
   List<CameraDescription>? cameras; // list of devices cameras
   XFile? image; // for captured image
+  late StreamSubscription stream;
+  List<ScrapbookTile> _display = List.empty(growable: true);
+  final CollectionReference scrapbookCollection =
+      FirebaseFirestore.instance.collection('scrapbooks');
 
   @override
   void initState() {
     findCameras();
+    findScrapbooks();
     super.initState();
   }
 
@@ -25,6 +35,7 @@ class _ARViewPageState extends State<ARViewPage> {
       print("NO CAMERAS FOUND!");
       return;
     }
+    print(cameras!.length);
     cameraController = CameraController(cameras![0], ResolutionPreset.max);
     cameraController!.initialize().then((value) {
       if (!mounted) return;
@@ -32,8 +43,44 @@ class _ARViewPageState extends State<ARViewPage> {
     });
   }
 
+  void findScrapbooks() async {
+    scrapbookCollection.snapshots().listen((event) {
+      for (var doc in event.docs) {
+        _display.add(ScrapbookTile(Scrapbook.fromDocument(doc)));
+      }
+    });
+  }
+
+  List<Widget> setDisplayStack() {
+    List<Widget> displayList = List.empty(growable: true);
+    displayList.add(cameraController == null
+        ? Center(child: Text("Loading Camera..."))
+        : Stack(
+            children: [
+              Center(
+                child: !cameraController!.value.isInitialized
+                    ? Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : CameraPreview(cameraController!),
+                widthFactor: 100.0,
+                heightFactor: 100.0,
+              )
+            ],
+          ));
+    for (var tile in _display) {
+      if (tile.scrapbook.public) {
+        // todo location
+        displayList.add(tile);
+      }
+    }
+    print(displayList.length);
+    return displayList;
+  }
+
   @override
   Widget build(BuildContext context) {
+    Stack stack = Stack(children: setDisplayStack());
     return Container(
         alignment: Alignment.center,
         margin: const EdgeInsets.fromLTRB(0, 5, 0, 10),
@@ -41,16 +88,6 @@ class _ARViewPageState extends State<ARViewPage> {
             color: Colors.white,
             border: Border.all(color: Colors.white),
             borderRadius: const BorderRadius.all(Radius.circular(20))),
-        child: cameraController == null
-            ? Center(child: Text("Loading Camera..."))
-            : Stack(
-                children: [
-                  !cameraController!.value.isInitialized
-                      ? Center(
-                          child: CircularProgressIndicator(),
-                        )
-                      : CameraPreview(cameraController!)
-                ],
-              ));
+        child: stack);
   }
 }
