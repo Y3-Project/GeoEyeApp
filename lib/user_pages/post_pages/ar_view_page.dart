26a0a2dbe9a293d30d65586a8f66a2c2da037d01
Jacob Turner <jacobmturner@gmail.com>
2023-03-13 10:31:27 +0000

    user can go onto the AR view and see through their camera
    you might want to note that getting this working on emulators might be difficult for you

--- lib/user_pages/post_pages/ar_view_page.dart
+++ lib/user_pages/post_pages/ar_view_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 class ARViewPage extends StatefulWidget {
@@ -9,16 +9,44 @@ class ARViewPage extends StatefulWidget {
 }
 
 class _ARViewPageState extends State<ARViewPage> {
+  CameraController? cameraController; // controller for camera
+  List<CameraDescription>? cameras; // list of devices cameras
+  XFile? image; // for captured image
+
+  @override
+  void initState() {
+    findCameras();
+    super.initState();
+  }
+
+  void findCameras() async {
+    cameras = await availableCameras();
+    if (cameras == null) {
+      print("NO CAMERAS FOUND!");
+      return;
+    }
+    cameraController = CameraController(cameras![0], ResolutionPreset.max);
+    cameraController!.initialize().then((value) {
+      if (!mounted) return;
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      margin: const EdgeInsets.fromLTRB(0, 5, 0, 10),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.white),
-          borderRadius: const BorderRadius.all(Radius.circular(20))),
-      child: Text("AR View"),
-    );
+        alignment: Alignment.center,
+        margin: const EdgeInsets.fromLTRB(0, 5, 0, 10),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.white),
+            borderRadius: const BorderRadius.all(Radius.circular(20))),
+        child: cameraController == null
+            ? Center(child: Text("Loading Camera..."))
+            : !cameraController!.value.isInitialized
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : CameraPreview(cameraController!));
   }
 }
