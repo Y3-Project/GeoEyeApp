2c44788f5d8edd8a9df191994798eb50259d6eca
03y <98747393+03y@users.noreply.github.com>
2023-03-19 18:25:47 +0000

    made it more apparent which username the user has selected

--- lib/user_authentication_widgets/create_account_page.dart
+++ lib/user_authentication_widgets/create_account_page.dart
@@ -105,36 +105,48 @@ class _SignUpPageState extends State<SignUpPage> {
           Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
             GestureDetector(
                 onTap: () {
-                  selectedUsername = username1;
+                  setState(() {
+                    selectedUsername = username1;
+                  });
                   ScaffoldMessenger.of(context).showSnackBar(snack1);
                 },
                 child: Text(username1,
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        fontSize: 14,
-                        color: Colors.lightBlue))),
+                        fontSize: selectedUsername == username1 ? 20 : 16,
+                        color: selectedUsername == username1
+                            ? Colors.green
+                            : Colors.lightBlue))),
             SizedBox(height: 25),
             GestureDetector(
                 onTap: () {
-                  selectedUsername = username2;
+                  setState(() {
+                    selectedUsername = username2;
+                  });
                   ScaffoldMessenger.of(context).showSnackBar(snack2);
                 },
                 child: Text(username2,
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        fontSize: 14,
-                        color: Colors.lightBlue))),
+                        fontSize: selectedUsername == username2 ? 20 : 16,
+                        color: selectedUsername == username2
+                            ? Colors.green
+                            : Colors.lightBlue))),
             SizedBox(height: 25),
             GestureDetector(
                 onTap: () {
-                  selectedUsername = username3;
+                  setState(() {
+                    selectedUsername = username3;
+                  });
                   ScaffoldMessenger.of(context).showSnackBar(snack3);
                 },
                 child: Text(username3,
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        fontSize: 14,
-                        color: Colors.lightBlue)))
+                        fontSize: selectedUsername == username3 ? 20 : 16,
+                        color: selectedUsername == username3
+                            ? Colors.green
+                            : Colors.lightBlue)))
           ]),
           TextFormField(
             controller: _controllerPassword,
