26fb3e60fa2234f2f3dcff30a216b144c416e05a
EvanBarker <eb2000@hw.ac.uk>
2023-03-21 14:04:59 +0000

    still not got the popup showing, not sure why

--- lib/scrapbook_widgets/scrapbook_tile.dart
+++ lib/scrapbook_widgets/scrapbook_tile.dart
@@ -1,9 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:popup_card/popup_card.dart';
 import 'package:flutter_app_firebase_login/scrapbook_widgets/scrapbook.dart';
 import 'package:flutter_app_firebase_login/scrapbook_widgets/scrapbook_posts_page.dart';
-import 'package:flutter_app_firebase_login/user_pages/other_user_widget.dart';
+//import 'package:flutter_app_firebase_login/user_pages/other_user_widget.dart';
 
 import '../user_pages/profile_page.dart';
 import '../util/util.dart';
@@ -33,13 +34,15 @@ class _ScrapbookTileState extends State<ScrapbookTile> {
         .collection("users")
         .doc(widget.scrapbook.creatorid.substring(7))
         .get()
-        .then((DocumentSnapshot snapshot) {
-      //if (snapshot.exists) {
-      profileUrl = snapshot.get("profilePicture");
-      print("1 + " + profileUrl);
-      print("1 + " + widget.scrapbook.creatorid.substring(7));
-      //}
-    })); //not realy sure what this does
+        .then(
+      (DocumentSnapshot snapshot) {
+        //if (snapshot.exists) {
+        profileUrl = snapshot.get("profilePicture");
+        //print("1 + " + profileUrl);
+        //print("1 + " + widget.scrapbook.creatorid.substring(7));
+        //}
+      },
+    )); //not realy sure what this does
     /*.where("uuid", isEqualTo: widget.scrapbook.creatorid.substring(7))
         .snapshots()
         .listen((event) {
@@ -76,6 +79,23 @@ class _ScrapbookTileState extends State<ScrapbookTile> {
       // default image file from images/default_image.png
       return Image.asset('images/default_image.png');
     }
+  }
+
+  Widget OtherUserPopUpItemBody() {
+    return Center(
+      child: Column(
+        children: [
+          CircleAvatar(
+              backgroundImage: AssetImage('images/default_avatar.png')),
+          Text("username"),
+          Text("Bio"),
+          Divider(),
+          Text("Scrapbook count"),
+          Text("Interaction count"),
+          Text("Interacted with count")
+        ],
+      ),
+    );
   }
 
   SnackBar reportedSnackBar = SnackBar(
@@ -161,8 +181,8 @@ class _ScrapbookTileState extends State<ScrapbookTile> {
   void initState() {
     super.initState();
     //getCurrentUserReference();
-    print("2 + " + profileUrl);
-    print("2 + " + widget.scrapbook.creatorid.substring(7));
+    //print("2 + " + profileUrl);
+    //print("2 + " + widget.scrapbook.creatorid.substring(7));
     //getProfilePic();
   }
 
@@ -171,54 +191,81 @@ class _ScrapbookTileState extends State<ScrapbookTile> {
     return Padding(
       padding: EdgeInsets.only(top: 8.0),
       child: Card(
-          shape: ContinuousRectangleBorder(
-              borderRadius: BorderRadius.circular(30)),
-          margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0),
-          child: FutureBuilder(
-              future: getCurrentUserReference(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                /*if (snapshot.connectionState == ConnectionState.done) {
+        shape:
+            ContinuousRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0),
+        child: FutureBuilder(
+            future: getCurrentUserReference(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              /*if (snapshot.connectionState == ConnectionState.done) {
                 if (snapshot.hasError) {
                   return Text('Error: ${snapshot.error}');
                 } else {
                   return Text('Result: ${snapshot.data}');
                 }
               } else {*/
-                return InkWell(
-                  borderRadius: BorderRadius.circular(8),
-                  onLongPress: () {
-                    showAlertDialog(context);
-                  },
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                          builder: (context) => ScrapbookPostsPage(
-                              scrapbook: this.widget.scrapbook)),
-                    );
-                  },
-                  child: ListTile(
-                    leading: InkWell(
-                        onTap: () {
-                          OtherUserPopUp();
-                          //popUpItemBody();
-                        },
-                        child: CircleAvatar(
-                            //backgroundImage: NetworkImage(profileUrl))
-                            //"https://firebasestorage.googleapis.com/v0/b/flutter-app-firebase-log-c1c41.appspot.com/o/images%2FKhrRphvHVAesRFxpMaePhkd8kJ93%2Fprofile_picture?alt=media&token=cd21af1b-c36d-4d6f-8f6b-9daf6791281c")),
-                            backgroundImage: profileUrl == ''
-                                ? Image(
-                                    image:
-                                        AssetImage('images/default_avatar.png'),
-                                  ).image
-                                : NetworkImage(profileUrl))),
-                    title: Text(widget.scrapbook.scrapbookTitle),
-                    subtitle: Text(widget.scrapbook.currentUsername),
-                    trailing: imageHandler(),
+              return InkWell(
+                borderRadius: BorderRadius.circular(8),
+                onLongPress: () {
+                  showAlertDialog(context);
+                },
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          ScrapbookPostsPage(scrapbook: this.widget.scrapbook),
+                    ),
+                  );
+                },
+                child: ListTile(
+                  leading: InkWell(
+                    onTap: () {
+                      //OtherUserPopUp();
+                      PopupItemLauncher(
+                        tag: 'otherUserPage',
+                        child: Material(
+                          color: Colors.black,
+                          elevation: 2,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(32),
+                          ),
+                          child: const Icon(
+                            color: Colors.white,
+                            Icons.add_rounded,
+                            size: 60,
+                          ),
+                        ),
+                        popUp: PopUpItem(
+                          padding: EdgeInsets.all(8),
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(32),
+                          ),
+                          elevation: 2,
+                          tag: 'OtherUserPage',
+                          child: OtherUserPopUpItemBody(),
+                        ),
+                      );
+                    },
+                    child: CircleAvatar(
+                      //backgroundImage: NetworkImage(profileUrl))
+                      //"https://firebasestorage.googleapis.com/v0/b/flutter-app-firebase-log-c1c41.appspot.com/o/images%2FKhrRphvHVAesRFxpMaePhkd8kJ93%2Fprofile_picture?alt=media&token=cd21af1b-c36d-4d6f-8f6b-9daf6791281c")),
+                      backgroundImage: profileUrl == ''
+                          ? Image(
+                              image: AssetImage('images/default_avatar.png'),
+                            ).image
+                          : NetworkImage(profileUrl),
+                    ),
                   ),
-                );
-              }
-              //},
-              )),
+                  title: Text(widget.scrapbook.scrapbookTitle),
+                  subtitle: Text(widget.scrapbook.currentUsername),
+                  trailing: imageHandler(),
+                ),
+              );
+            }
+            //},
+            ),
+      ),
     );
   }
 }
